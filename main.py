8a4885195a78a7e19602fa984f7085669873827e
gizmo <rotaru_robert97@yahoo.com>
2022-07-28 23:46:10 +0300

    ceva

---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+import time
+import re
+from bs4 import BeautifulSoup
+import psycopg2
+
+
+root_url = 'https://ak447.anime-kage.eu/ak/anime/'
+
+conn=psycopg2.connect(
+    database="exampledb",
+    user="docker",
+    password="docker",
+    host="database"
+)
+
+#https://ak494.anime-kage.eu/ak/anime/kaguya-sama-wa-kokurasetai-ultra-romantic
+#https://ak447.anime-kage.eu/ak/anime/kaguyasama-wa-kokurasetai-ultra-romantic
+
+cur = conn.cursor()
+cur.execute ('select * from "tblShows" where name <> \'One Piece\'')
+showRows= cur.fetchall()
+for showRow in showRows:
+    url=root_url+re.sub('[^a-zA-Z0-9 \-\n\.]', '',showRow[1]).strip().replace(' ','-').lower()
+    end=int(showRow[2])
+    start = 1
+    for start in range(end+1):
+        time.sleep(0.3)
+        # Connect to the URL
+        response = requests.get(url+'/'+str(start))
+        # Parse HTML and save to BeautifulSoup object¶
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        found_fembed = ''
+        found_mega = ''
+        found_lare = ''
+        found_up=''
+        found_sendvid = ''
+        found_vtube = ''
+
+        for one_a_tag in soup.findAll('iframe'):
+            if "fembed" in one_a_tag.get('data-src'):
+                found_fembed = one_a_tag.get('data-src')
+            elif "mega.nz" in one_a_tag.get('data-src'):
+                found_mega = one_a_tag.get('data-src')
+            elif "vtube.to" in one_a_tag.get('data-src'):
+                found_vtube = one_a_tag.get('data-src')
+            elif "streamlare.com" in one_a_tag.get('data-src'):
+                found_lare = one_a_tag.get('data-src')
+            elif "upstream.to" in one_a_tag.get('data-src'):
+                found_up = one_a_tag.get('data-src')
+            elif "sendvid.com" in one_a_tag.get('data-src'):
+                found_up = one_a_tag.get('data-src')
+
+        if found_fembed:
+            cur.execute("insert into \"tblLinks\" (show_id,episode,link,downloaded) VALUES ("+str(showRow[0])+","+str(start)+",\'"+found_fembed+"\',FALSE)")
+        elif found_mega:
+            cur.execute("insert into \"tblLinks\" (show_id,episode,link,downloaded) VALUES ("+str(showRow[0])+","+str(start)+",\'"+found_mega+"\',FALSE)")
+        elif found_vtube:
+            cur.execute("insert into \"tblLinks\" (show_id,episode,link,downloaded) VALUES ("+str(showRow[0])+","+str(start)+",\'"+found_vtube+"\',FALSE)")
+        elif found_up:
+            cur.execute("insert into \"tblLinks\" (show_id,episode,link,downloaded) VALUES ("+str(showRow[0])+","+str(start)+",\'"+found_up+"\',FALSE)")
+        elif found_lare:
+            cur.execute("insert into \"tblLinks\" (show_id,episode,link,downloaded) VALUES ("+str(showRow[0])+","+str(start)+",\'"+found_lare.replace('/e/','/v/')+"\',FALSE)")
+
+conn.commit()
+cur.close()
+conn.close()
+
---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "amzLinux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amzLinux.id
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    EOF
+
+  tags = {
+    Name = "CevaExemplu"
+  }
+}
+
